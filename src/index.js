4b36b6d7d193f45a5b7ceeffd2d9596152dabcab
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-09-30 16:39:13 -0400

    configure netlifty and axios for production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import axios from "axios";
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 import "index.scss";
 
 import Application from "components/Application";
