f9a0a61fe9d28a849a63d789c40cfbeab06a129b
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-09-30 16:45:19 -0400

    reorder imports to top

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "index.scss";
+import Application from "components/Application";
 
 import axios from "axios";
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
-
-import "index.scss";
-
-import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
